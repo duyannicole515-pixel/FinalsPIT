7f2ae47c650af52726838ac7e8d9e4836ecca538
Alexandra Nicole Duyan <duyan.nicole515@gmail.com>
2026-05-10 08:52:41 +0800

    Added a 'Exit Game' and 'Play Again' button

--- pythonTicTacToe.py
+++ pythonTicTacToe.py
@@ -166,5 +166,10 @@ b1.grid(row=0, column=0); b2.grid(row=0, column=1); b3.grid(row=0, column=2)
 b4.grid(row=1, column=0); b5.grid(row=1, column=1); b6.grid(row=1, column=2)
 b7.grid(row=2, column=0); b8.grid(row=2, column=1); b9.grid(row=2, column=2)
 
+againButton = Button(root, text="Play Again", command=reset_game)
+againButton.grid(row=3, column=0, pady=10)
+
+exitButton = Button(root, text="Exit Game", command=exit_game)
+exitButton.grid(row=3, column=2, pady=10)
 
 root.mainloop()
