969fa4ccdbc2e5aa28857e378a5dc0e0e55de877
Alexandra Nicole Duyan <duyan.nicole515@gmail.com>
2026-05-12 09:04:50 +0800

    Player's name input

--- pythonTicTacToe.py
+++ pythonTicTacToe.py
@@ -7,6 +7,40 @@ root.title('Tic Tac Toe')
 playerX=True
 count=0 
 
+def players_name():
+    try:
+        p1=player1_entry.get()
+        p2=player2_entry.get()
+
+        if not p1.replace(" ","").isalpha() or not p2.replace(" ","").isalpha():
+            raise ValueError("NNAME MUST ONLY BE LETTERS")
+        
+        if p1.strip()==""or p2.strip()=="":
+            raise ValueError("Please dont't leave it blank.")
+
+        for b in [b1, b2, b3, b4, b5, b6, b7, b8, b9]:
+            b.config(state=NORMAL)
+
+        startButton.config(state=DISABLED)
+        messagebox.showerror("Game Ready", f"Match Started: {p1} vs {p2}")
+
+    except ValueError as err:
+        messagebox.showerror(err)
+
+name_frame=Frame(root)
+name_frame.grid(row=0, column=0, columnspan=3, pady=10)
+
+Label(name_frame, text="Player X Name:").grid(row=0, column=0)
+player1_entry=Entry(name_frame)
+player1_entry.grid(row=0, column=1)
+
+Label(name_frame, text="Player O Name:").grid(row=1, column=0)
+player2_entry=Entry(name_frame)
+player2_entry.grid(row=1, column=1)
+
+startButton=Button(name_frame, text="Start Game", command=players_name)
+startButton.grid(row=2, column=0, columnspan=2, pady=5)
+
 def save_to_file(winner_text):
     try:
         with open("winners_record.txt", "a") as f:
@@ -25,7 +59,7 @@ def reset_game():
     count = 0
     all_buttons = [b1, b2, b3, b4, b5, b6, b7, b8, b9]
     for button in all_buttons:
-        button.config(text=" ", bg="SystemButtonFace", state=NORMAL)
+        button.config(text=" ", bg="SystemButtonFace", state=DISABLED)
 
 def exit_game():
     root.destroy()
@@ -34,101 +68,105 @@ def winnerchecker():
     global winner
     winner = False
 
+    p1=player1_entry.get()
+    p2=player2_entry.get()
+
+
     if b1["text"]=="X" and b2["text"]=="X" and b3["text"]=="X":
         b1.config(bg="green"); b2.config(bg="green"); b3.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player X, you win!!")
+        messagebox.showinfo("Tic Tac Toe",)
         save_to_file("Player X Wins!!!!")
         disable_other_buttons()
     elif b4["text"]=="X" and b5["text"]=="X" and b6["text"]=="X":
         b4.config(bg="green"); b5.config(bg="green"); b6.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player X, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p1}, you win!!")
         save_to_file("Player X Wins!!!!")
         disable_other_buttons()
     elif b7["text"]=="X" and b8["text"]=="X" and b9["text"]=="X":
         b7.config(bg="green"); b8.config(bg="green"); b9.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player X, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p1}, you win!!")
         save_to_file("Player X Wins!!!!")
         disable_other_buttons()
     elif b1["text"]=="X" and b4["text"]=="X" and b7["text"]=="X":
         b1.config(bg="green"); b4.config(bg="green"); b7.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player X, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p1}, you win!!")
         save_to_file("Player X Wins!!!!")
         disable_other_buttons()
     elif b2["text"]=="X" and b5["text"]=="X" and b8["text"]=="X":
         b2.config(bg="green"); b5.config(bg="green"); b8.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player X, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p1}, you win!!")
         save_to_file("Player X Wins!!!!")
         disable_other_buttons()
     elif b3["text"]=="X" and b6["text"]=="X" and b9["text"]=="X":
         b3.config(bg="green"); b6.config(bg="green"); b9.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player X, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p1}, you win!!")
         save_to_file("Player X Wins!!!!")
         disable_other_buttons()
     elif b1["text"]=="X" and b5["text"]=="X" and b9["text"]=="X":
         b1.config(bg="green"); b5.config(bg="green"); b9.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player X, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p1}, you win!!")
         save_to_file("Player X Wins!!!!")
         disable_other_buttons()
     elif b3["text"]=="X" and b5["text"]=="X" and b7["text"]=="X":
         b3.config(bg="green"); b5.config(bg="green"); b7.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player X, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p1}, you win!!")
         save_to_file("Player X Wins!!!!")
         disable_other_buttons()
 
     elif b1["text"]=="O" and b2["text"]=="O" and b3["text"]=="O":
         b1.config(bg="green"); b2.config(bg="green"); b3.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player O, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p2}, you win!!")
         save_to_file("Player O Wins!!!!")
         disable_other_buttons()
     elif b4["text"]=="O" and b5["text"]=="O" and b6["text"]=="O":
         b4.config(bg="green"); b5.config(bg="green"); b6.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player O, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p2}, you win!!")
         save_to_file("Player O Wins!!!!")
         disable_other_buttons()
     elif b7["text"]=="O" and b8["text"]=="O" and b9["text"]=="O":
         b7.config(bg="green"); b8.config(bg="green"); b9.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player O, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p2}, you win!!")
         save_to_file("Player O Wins!!!!")
         disable_other_buttons()
     elif b1["text"]=="O" and b4["text"]=="O" and b7["text"]=="O":
         b1.config(bg="green"); b4.config(bg="green"); b7.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player O, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p2}, you win!!")
         save_to_file("Player O Wins!!!!")
         disable_other_buttons()
     elif b2["text"]=="O" and b5["text"]=="O" and b8["text"]=="O":
         b2.config(bg="green"); b5.config(bg="green"); b8.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player O, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p2}, you win!!")
         save_to_file("Player O Wins!!!!")
         disable_other_buttons()
     elif b3["text"]=="O" and b6["text"]=="O" and b9["text"]=="O":
         b3.config(bg="green"); b6.config(bg="green"); b9.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player O, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p2}, you win!!")
         save_to_file("Player O Wins!!!!")
         disable_other_buttons()
     elif b1["text"]=="O" and b5["text"]=="O" and b9["text"]=="O":
         b1.config(bg="green"); b5.config(bg="green"); b9.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player O, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p2}, you win!!")
         save_to_file("Player O Wins!!!!")
         disable_other_buttons()
     elif b3["text"]=="O" and b5["text"]=="O" and b7["text"]=="O":
         b3.config(bg="green"); b5.config(bg="green"); b7.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "Congrats Player O, you win!!")
+        messagebox.showinfo("Tic Tac Toe", f"Congrats {p2}, you win!!")
         save_to_file("Player O Wins!!!!")
         disable_other_buttons()
     
@@ -152,24 +190,24 @@ def b_click(b):
     else:
         messagebox.showerror("Tic Tac Toe", "That box has already been selected.")
 
-b1 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b1))
-b2 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b2))
-b3 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b3))
-b4 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b4))
-b5 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b5))
-b6 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b6))
-b7 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b7))
-b8 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b8))
-b9 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b9))
+b1 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", state=DISABLED, command=lambda: b_click(b1))
+b2 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", state=DISABLED, command=lambda: b_click(b2))
+b3 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", state=DISABLED, command=lambda: b_click(b3))
+b4 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", state=DISABLED, command=lambda: b_click(b4))
+b5 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", state=DISABLED, command=lambda: b_click(b5))
+b6 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", state=DISABLED, command=lambda: b_click(b6))
+b7 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", state=DISABLED, command=lambda: b_click(b7))
+b8 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", state=DISABLED, command=lambda: b_click(b8))
+b9 = Button(root, text=" ", font=("Verdana", 20), height=3, width=6, bg="SystemButtonFace", state=DISABLED, command=lambda: b_click(b9))
 
-b1.grid(row=0, column=0); b2.grid(row=0, column=1); b3.grid(row=0, column=2)
-b4.grid(row=1, column=0); b5.grid(row=1, column=1); b6.grid(row=1, column=2)
-b7.grid(row=2, column=0); b8.grid(row=2, column=1); b9.grid(row=2, column=2)
+b1.grid(row=1, column=0); b2.grid(row=1, column=1); b3.grid(row=1, column=2)
+b4.grid(row=2, column=0); b5.grid(row=2, column=1); b6.grid(row=2, column=2)
+b7.grid(row=3, column=0); b8.grid(row=3, column=1); b9.grid(row=3, column=2)
 
 againButton = Button(root, text="Play Again", command=reset_game)
-againButton.grid(row=3, column=0, pady=10)
+againButton.grid(row=4, column=0, pady=10)
 
 exitButton = Button(root, text="Exit Game", command=exit_game)
-exitButton.grid(row=3, column=2, pady=10)
+exitButton.grid(row=4, column=2, pady=10)
 
 root.mainloop()
